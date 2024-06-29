d88952f16b02749779fd807062f99561a34f8db0
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-06-30 00:48:00 +0200

    1.0

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import modules.configuration as configuration
 import modules.instagram as instagram
 import modules.webhook as webhook
+import modules.colors as colors
 import os, time, sys
 
 if os.name != "nt":
      os.environ.get("TERM")
 
 if not os.path.isfile("config.cfg"):
-     print("Running for first time! Creating new config")
+     print(colors.red + "Running for first time! Creating new config" + colors.reset)
      configuration.create()
 
 if len(sys.argv) > 1:
@@ -18,26 +19,28 @@ if len(sys.argv) > 1:
      else:
           print(f"Unknown argument! Got {arg}. Continuing as usual.")
 
-print("Loading configuration!")
+print(colors.yellow + "Loading configuration!" + colors.reset)
 config = configuration.load()     
 
-print("Startup successful!")
+print(colors.green + "Startup successful!" + colors.reset)
 
-latest_post_id = None
-latest_story_id = None
+latest_post_id = configuration.get_value("Instagram", "latest_post_id")
+latest_story_id = configuration.get_value("Instagram", "latest_story_id")
 
 while True:
     post_id, post_url, post_caption = instagram.fetch_latest_post()
     if post_id != latest_post_id:
         latest_post_id = post_id
-        print("Got new post!")
-        webhook.send_webhook(id=0,name="Rock for People 2025 - Post", caption=post_caption, url=post_url)
+        configuration.save("Instagram", "latest_post_id", str(latest_post_id))
+        print(colors.yellow + "Got new post!" + colors.reset)
+        webhook.send_webhook(name="Rock for People 2025 - Post", type="Post", ig_id=post_id, caption=post_caption, url=post_url,embed_color="ffA500")
 
     story_id, story_url, story_caption = instagram.fetch_latest_story()
-    if story_id != latest_story_id:
+    if story_id != latest_story_id and story_id != None:
         latest_story_id = story_id
-        print("Got new story!")
-        webhook.send_webhook(id=0,name="Rock for People 2025 - Story", caption=story_caption, url=story_url)
+        configuration.save("Instagram", "latest_story_id", str(latest_story_id))
+        print(colors.yellow + "Got new story!" + colors.reset)
+        webhook.send_webhook(name="Rock for People 2025 - Story", type="Story",ig_id=story_id, caption=story_caption, url=story_url,embed_color="ffA500")
 
     # Check for new posts and stories every 10 minutes
-    time.sleep(600)
+    time.sleep(120)
--- modules/webhook.py
+++ modules/webhook.py
@@ -1,16 +1,21 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import modules.configuration as configuration
-from modules.f_time import remaining_time
 
-def send_webhook(id, name, ig_id, caption, url, embed_color):
-    config = configuration.load()
+def send_webhook(name, type, ig_id, caption, url, embed_color):
+    role = configuration.get_value("General", "role_id")
+    webhook_url = configuration.get_value("General", "webhook")
     #Create new instance
-    webhook = DiscordWebhook(url=f'{config["General"]["webhook"]}', content=f'<@&{config[id]["role_id"]}>')
+    webhook = DiscordWebhook(url=webhook_url, content=f'<@&{role}>')
 
     #Configure message
-    embed = DiscordEmbed(title=f'Novej Instagram Post/Story', description=f'Caption: {caption}\n URL: {url}', color=embed_color)
-    embed.set_author(name=name) 
-    embed.set_footer(f"Doufám, že tohle nebyl zbytečnej ping, Instagram ID: {ig_id}")
+    if type == "Post":
+        embed = DiscordEmbed(title=f'Nový Instagram Post', description=f'Caption: {caption}', color=embed_color, rate_limit_retry=True)
+    if type == "Story":
+        embed = DiscordEmbed(title=f'Nová Instagram Story', description=f'Caption: {caption}', color=embed_color, rate_limit_retry=True)
+    #embed.set_author(name=name) 
+    embed.set_image(url=url)
+    embed.set_footer(f"Doufám, že tohle nebyl zbytečnej ping.\nInstagram ID: {ig_id}")
+
     #Add it to message
     webhook.add_embed(embed)
 
