64bb44dcded972e2113e5e1c882784fca1a0e4bb
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-07-12 22:48:08 +0200

    Update docker_handler.py
    Added discord module for sending shutdown messages with error

--- modules/docker_handler.py
+++ modules/docker_handler.py
@@ -1,4 +1,5 @@
 import os, signal
-
-def shutdown_container():
+import modules.discord as discord
+def shutdown_container(error="None"):
+    discord.send_webhook(name="Rock for People 2025 - Error", type="error", ig_id=f"Error", caption=f"Error:\n {error}", embed_color="ff0000")
     os.kill(os.getpid(), signal.SIGTERM)
