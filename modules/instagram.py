e01a68ac5563bfde5a92f69fa673dd0dec316994
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-07-12 22:47:45 +0200

    Changed max_fails logic
    Due to human error we were importing fails as value from configuration file, that meant we were instantly shutting down instead of trying for X times.

--- modules/instagram.py
+++ modules/instagram.py
@@ -8,21 +8,27 @@ configuration.load()
 instagram_account = configuration.get_value("Instagram", "account")
 instagram_username = configuration.get_value("Instagram", "username")
 instagram_password = configuration.get_value("Instagram", "password")
-fails = configuration.get_value("General", "max_fails")
 
-L = instaloader.Instaloader(iphone_support=False, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+max_fails = int(configuration.get_value("General", "max_fails"))
+fails = 0
+
+L = instaloader.Instaloader(iphone_support=False,save_metadata=False,fatal_status_codes=False, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+profile = None
+
 try:
     L.login(instagram_username, instagram_password)
 except instaloader.BadCredentialsException:
     print(colors.red + f"Login failed. Looks like Instagram flagged this account :(. Let's sleep for 30min." + colors.reset) 
-        
     #Check if we failed 3 times, when True shutdown this container.
-    if fails == 3:
-        docker.shutdown_container()
-    fails = fails + 1
-    time.sleep(1800) #Wait for a 30min.
+    if fails == max_fails:
+        docker.shutdown_container(error="Login failed. Looks like Instagram flagged this account :(")
+    else:
+        fails = fails + 1
+        time.sleep(1800) #Wait for a 30min.
+        L.login(instagram_username, instagram_password)
+
 def fetch_latest_post():
-    profile = instaloader.Profile.from_username(L.context, instagram_account)
+    global profile
     posts = list(profile.get_posts())
     if posts:
         latest_post = posts[0]
@@ -31,9 +37,15 @@ def fetch_latest_post():
 
 # Function to fetch the latest story
 def fetch_latest_story():
-    profile = instaloader.Profile.from_username(L.context, instagram_account)
+    global profile
     stories = L.get_stories(userids=[profile.userid])
     for story in stories:
         for item in story.get_items():
             return item.shortcode, item.url, item.caption
     return None, None, None
+
+def update_profile():
+    global profile
+    print(colors.yellow + "Updating profile" + colors.reset)
+    profile = instaloader.Profile.from_username(L.context, instagram_account)
+    print(colors.green + "Profile updated!" + colors.reset)
